c6f2ce08741726e71b20f03184e73213168e1066
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2021-11-19 19:02:31 -0800

    // added instructions

--- findAnagarams.js
+++ findAnagarams.js
@@ -1,3 +1,14 @@
+
+/*
+
+This function identifies anagram(s) of a word in a given array.
+For example:
+Word: Anna      Array: ['abcd', 'aann', 'nana', 'naaa', 'nnaa']
+The anagrams in the array are aann, nana, nnaa. It will return an array of these anagrams.
+If there are no anagrams, it will return an empty array []
+
+*/
+
 function anagrams(word, words) {
     const template = word.split('').sort().join('');
     const array = [];
