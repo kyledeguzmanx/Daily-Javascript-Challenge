04b864fd5344c27c504b66afa1f2e45daa6b05f1
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2021-12-22 21:44:47 -0800

    new script

---
+++ reactHelloWorld.js
@@ -0,0 +1,7 @@
+ReactDOM.render(<h1>Hello, world!</h1>,
+    document.getElementById('divInDOM'));
+
+
+const helloMessage = (<h1>Hello World</h1>);
+
+ReactDOM.render(helloMessage, getElementById("divInDOM"));
