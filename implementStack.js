56f1533d9dbc5760e31cade1af775479a5f1eacd
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-02-17 23:33:30 -0800

    new push function

--- implementStack.js
+++ implementStack.js
@@ -1,7 +1,16 @@
+/*
+    Javascript Implementation of a Stack
+
+*/
+
 var Stack = function(){
     this.counter = 0;
     this.dataStructure = {};
 
+    this.push() = (item) => {
+        this.dataStructure[this.counter] = item;
+        this.counter++;
+    }
     
 
 }
