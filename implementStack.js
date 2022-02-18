2279718449838fd6beb79e6fec37a4dc86620779
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-02-17 23:35:24 -0800

    new pop function

--- implementStack.js
+++ implementStack.js
@@ -7,10 +7,20 @@ var Stack = function(){
     this.counter = 0;
     this.dataStructure = {};
 
-    this.push() = (item) => {
+    this.push = (item) => {
         this.dataStructure[this.counter] = item;
         this.counter++;
-    }
-    
+    };
 
+    this.pop = () => {
+        if(this.counter == 0){
+            return undefined;
+        }
+        else{
+            this.counter--;
+            var result = this.dataStructure[this.counter];
+            delete this.dataStructure[this.counter];
+            return result;
+        }
+    }
 }
