f90084a8b54042d18dcf9955ff230167b915766c
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-02-19 23:25:26 -0800

    updated

--- implementBST.js
+++ implementBST.js
@@ -10,4 +10,17 @@ class BinarySearchTree{
     constructor(){
         this.root = null;
     }
+
+    add(data){
+        const node = this.root;
+        if (!node){
+            this.root = new Node(data);
+            return;
+        }
+        else{
+            const searchTree = (node) => {
+                
+            }
+        }
+    }
 }
