4c02442a3e04c23d06ea8bc5d195058b750257b4
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-02-21 17:20:52 -0800

    Update reactClass2.js

--- reactClass2.js
+++ reactClass2.js
@@ -21,3 +21,10 @@ class Welcome extends React.Component {
     return <h1>Hello, {this.props.name}</h1>;
   }
 }
+
+//----------------------------
+class Welcome extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
