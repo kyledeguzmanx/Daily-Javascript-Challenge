b68181c239f2b6827517ffcd25abcb029efb3d99
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-06 13:19:50 -0800

    Update reactMapFunc.js

--- react/reactMapFunc.js
+++ react/reactMapFunc.js
@@ -42,3 +42,24 @@ class MyToDoList extends React.Component {
     );
   }
 }
+/**/
+const frontEndFrameworks = [
+  'React',
+  'Angular',
+  'Ember',
+  'Knockout',
+  'Backbone',
+  'Vue'
+];
+
+function Frameworks() {
+  const renderFrameworks = frontEndFrameworks.map((framework,index) => <li key={index}>{framework}</li>)
+  return (
+    <div>
+      <h1>Popular Front End JavaScript Frameworks</h1>
+      <ul>
+        {renderFrameworks}
+      </ul>
+    </div>
+  );
+};
