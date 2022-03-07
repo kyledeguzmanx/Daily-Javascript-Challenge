2e37a4e01c231a9254380464ac9ecaa62feedda8
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-06 23:44:02 -0800

    Update sendFile.js

--- express/sendFile.js
+++ express/sendFile.js
@@ -4,3 +4,11 @@ var app = express();
 app.get("/", (request, response)=>{
   response.sendFile(__dirname + "/views/index.html");
 })
+
+app.get("/json", (request, response) => {
+    if(process.env['MESSAGE_STYLE'] === "uppercase")
+      response.json({"message" : "HELLO JSON"})
+    else{
+      response.json({"message" : "Hello json"})
+    }
+})
