e6dd021fc35da13fd43d3252722e61f047dcc146
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-06 20:40:49 -0800

    Update reactAutoInput.jsx

--- react/reactAutoInput.jsx
+++ react/reactAutoInput.jsx
@@ -25,3 +25,43 @@ class ControlledInput extends React.Component {
     );
   }
 };
+/*----------------*/
+class DisplayMessages extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      messages: []
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.submitMessage = this.submitMessage.bind(this);
+  }
+  handleChange(event){
+    this.setState({
+      input: event.target.value,
+      messages: this.state.messages
+    })
+  }
+  submitMessage(){
+    this.setState({ 
+      
+      messages: [...this.state.messages, this.state.input],
+      input:""
+    });
+  }
+
+  render() {
+    const items = this.state.messages.map((item,index) => <li key={index}>{item}</li>)
+    return (
+      <div>
+        <h2>Type in a new Message:</h2>
+        <input onChange={this.handleChange} value={this.state.input}/>
+        <button onClick={this.submitMessage}>Add message</button>
+        <ul>
+          {items}
+        </ul>
+        
+      </div>
+    );
+  }
+};
