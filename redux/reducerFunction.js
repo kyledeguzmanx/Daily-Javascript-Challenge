2625fab5b22f7ec4572a4cd611baa9c73e1f8a27
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-06 16:54:09 -0800

    Update reducerFunction.js

--- redux/reducerFunction.js
+++ redux/reducerFunction.js
@@ -57,3 +57,39 @@ const logoutUser = () => {
     type: 'LOGOUT'
   }
 };
+/*----------------------*/
+const defaultState = {
+  authenticated: false
+};
+
+const authReducer = (state = defaultState, action) => {
+  switch(action.type){
+    case "LOGIN":
+      return {
+        authenticated: true
+      }
+    break;
+    case "LOGOUT":
+      return {
+        authenticated: false
+      }
+    break;
+    default:
+      return state;
+    break;
+  }
+}
+
+const store = Redux.createStore(authReducer);
+
+const loginUser = () => {
+  return {
+    type: 'LOGIN'
+  }
+};
+
+const logoutUser = () => {
+  return {
+    type: 'LOGOUT'
+  }
+};
