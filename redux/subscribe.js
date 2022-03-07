2b7cd4eeda670c6d1362cb6f0b91ac0052330f8e
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-06 17:02:37 -0800

    Create subscribe.js

---
+++ redux/subscribe.js
@@ -0,0 +1,31 @@
+const ADD = 'ADD';
+
+const reducer = (state = 0, action) => {
+  switch(action.type) {
+    case ADD:
+      return state + 1;
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(reducer);
+
+// Global count variable:
+let count = 0;
+
+// Change code below this line
+
+
+const incrementCount = () => {
+  count++;
+}
+store.subscribe(incrementCount) //subscribe listener to store; called whenever an action is dispatched
+// Change code above this line
+
+store.dispatch({type: ADD});
+console.log(count);
+store.dispatch({type: ADD});
+console.log(count);
+store.dispatch({type: ADD});
+console.log(count);
