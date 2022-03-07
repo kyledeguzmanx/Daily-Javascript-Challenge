56e039aefe74e61ec9f5c688952c6d49f58ec79a
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-06 19:30:22 -0800

    Update reducerFunction.js

--- redux/reducerFunction.js
+++ redux/reducerFunction.js
@@ -93,3 +93,28 @@ const logoutUser = () => {
     type: 'LOGOUT'
   }
 };
+/*-----------------*/
+const ADD_NOTE = 'ADD_NOTE';
+
+const notesReducer = (state = 'Initial State', action) => {
+  switch(action.type) {
+    case ADD_NOTE:
+        return action.text
+      break;
+    default:
+      return state;
+  }
+};
+
+const addNoteText = (note) => {
+    return {
+      type: ADD_NOTE,
+      text: note
+    }
+};
+
+const store = Redux.createStore(notesReducer);
+
+console.log(store.getState());
+store.dispatch(addNoteText('Hello!'));
+console.log(store.getState());
