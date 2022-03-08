88bf8a938ad48adc8aab63014b29a893946ea6ff
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-07 23:28:11 -0800

    Update removeDocument.js

--- mongodb/removeDocument.js
+++ mongodb/removeDocument.js
@@ -7,3 +7,15 @@ const removeById = (personId, done) => {
     done(null, data)
   })
 };
+
+const removeManyPeople = (done) => {
+  const nameToRemove = "Mary";
+
+  Person.remove({name: nameToRemove}, (error,data) => {
+    if(error){
+      console.error(error)
+    }
+    done(null,data);
+  });
+};
+
