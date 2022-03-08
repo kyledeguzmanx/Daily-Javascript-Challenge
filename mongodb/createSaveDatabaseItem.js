21b367a50da0f8d8f48c48fcea0d7609b4cd6ba4
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-07 22:36:11 -0800

    Create createSaveDatabaseItem.js

---
+++ mongodb/createSaveDatabaseItem.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const mySecret = process.env['PASSWORD']
+mongoose.connect(mySecret);
+
+
+const personSchema  = new Schema({
+  name: {type: String, required: true},
+  age: Number,
+  favoriteFoods: [String]
+});
+let Person = mongoose.model("Person", personSchema); //creates model called person
+
+/*
+let personSchema = new Schema({
+  name: {type: String}, //name: {type: String, required:true}
+  age: Number,
+  favoriteFoods:[{type: String}]
+  //also Boolean
+});*/
+
+const createAndSavePerson = (done) => {
+  let kyleDeGuzman = new Person({
+    name: "Kyle DeGuzman",
+    age: 21,
+    favoriteFoods: ["Mangos", "Fried Tofu", "Kiwi Starfruit Juice"]
+  })
+  kyleDeGuzman.save((error, data)=>{
+    if(error){
+      return console.error(error);
+    }
+    done(null,data);
+  })
+};
