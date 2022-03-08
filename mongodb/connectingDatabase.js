4b431dd802be106a4cce09cd225200d3de83957d
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-07 22:07:08 -0800

    Create connectingDatabase.js

---
+++ mongodb/connectingDatabase.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://<username>:<password>@cluster0.iejqx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+
+/*
+CREATE AN ORGANIZAITON
+CREATE A PROJECT
+CREATE A SHARED CLUSTER
+ADD DATABASE ACCESS INFO (USERNAME + PASSWORD)
+CONNECT AND INPUT CORRECT PASSWORD; USERNAME AND DATABASE ARE ALREADY GIVEN
+*/
