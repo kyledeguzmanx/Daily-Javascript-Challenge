fd263d9c8a106ed9a40f9a657cc52bb5d8db3055
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-07 22:20:11 -0800

    Create defineSchema.js

---
+++ mongodb/defineSchema.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+mongoose.connect("mongodb+srv://kyle:kylee@cluster0.iejqx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"); 
+/*CREDENTIALS ARE NOW INVALID DUE TO EXPOSURE*/
+
+
+const personSchema  = new Schema({
+  name: {type: String, required: true},
+  age: Number,
+  favoriteFoods: [String]
+});
+let Person = mongoose.model("Person", personSchema);
+
+/*
+let personSchema = new Schema({
+  name: {type: String}, //name: {type: String, required:true}
+  age: Number,
+  favoriteFoods:[{type: String}]
+  //also Boolean
+});*/
