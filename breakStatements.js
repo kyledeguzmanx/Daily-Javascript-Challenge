4dc62a3aae27ef74408793d95b3b0b3fe48836ed
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-19 23:45:16 -0700

    Create breakStatements.js

---
+++ breakStatements.js
@@ -0,0 +1,19 @@
+for (var i = 0; i < 100; i++) {
+  if (i === 5) {
+    break;
+  }
+  console.log('Number is ', i);
+}
+
+console.log('Yay');
+/*
+
+
+Number is 0
+Number is 1
+Number is 2
+Number is 3
+Number is 4
+Yay
+
+*/
