c3ccb53b8d1b580517854e71c1b96338ec47b139
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-23 22:14:15 -0700

    onClick

---
+++ eventsBasics.js
@@ -0,0 +1,6 @@
+const sampleElement = document.getElementById("sampleElement");
+const anotherElement = document.querySelector("h1");
+
+sampleElement.addEventListener("onclick", ()=> {
+  sampleElement.style.backgroundColor = blue;
+});
