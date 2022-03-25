0c2ca09c773ab577a7f0a0d2a4fa135413e57160
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-24 22:00:16 -0700

    Update extractDomainFromURL.js

--- extractDomainFromURL.js
+++ extractDomainFromURL.js
@@ -1,6 +1,17 @@
-function domainName(url){
-  if(url[0] == "h")
-    return (url.slice(url.indexOf("://")+3,url.indexOf(".")))
-else if(url[0] == "w")
-    return (url.slice(url.indexOf("www.")+4,url.lastIndexOf(".")))
+
+const url = " http://www.1ylun3q07sm7fj7omb.com/img/"
+console.log(url);
+// start of string        |       url.indexOf("://")+3
+// end of string          | url.indexOf(".com")
+//url[url.indexOf(".com")] start of .com
+
+if(url.includes("www.")){
+   console.log(url.slice(url.indexOf("www.")+4,url.lastIndexOf("."))) 
 }
+else if(url.includes("http")){
+    console.log(url.slice(url.indexOf("://")+3,url.indexOf(".")))
+}
+else if(!url.includes("www." && !url.includes("http"))){
+    console.log(url.slice(0, url.indexOf(".")))
+}
+    
