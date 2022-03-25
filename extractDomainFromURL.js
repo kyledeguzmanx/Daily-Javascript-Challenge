d675bdffafe47279ad1aa2fb2275a15b73972c5d
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-24 23:29:26 -0700

    Update extractDomainFromURL.js

--- extractDomainFromURL.js
+++ extractDomainFromURL.js
@@ -30,4 +30,10 @@ console.log(domainName("https://www.codewars.com/kata/514a024011ea4fb54200004b/s
 
 /*
 ALT SOLUTION
+function domainName(url){
+  url = url.replace("https://", '');
+  url = url.replace("http://", '');
+  url = url.replace("www.", '');
+  return url.split('.')[0];
+};
 */
