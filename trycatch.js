8fd39acac2a8c0cef5f5ad1ed48900fa356fdbbd
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-06-01 23:08:41 -0700

    Update trycatch.js

--- trycatch.js
+++ trycatch.js
@@ -1,3 +1,11 @@
+/*
+ - not to be used instead of if-else
+ - use sparingly
+ - this is for scenarios where errors may arise that are beyond the scope of your control; your code is dependent on a third-party not fulfilling the role
+ it is expected to
+ - if errors are within the scope of your control, you should fix that without using try-catch
+ - this is not for input validation
+*/
 try{ //try this block of code
 
 }
